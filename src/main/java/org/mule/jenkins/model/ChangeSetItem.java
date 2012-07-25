e3ff012fe2c0bb252544de098a597110650385fa
Dario Carabajal <adcarabajal@gmail.com>
2012-07-25 16:59:02 -0300

    changes to fulfill this issue CLDCONNECT-345

---
+++ src/main/java/org/mule/jenkins/model/ChangeSetItem.java
@@ -0,0 +1,11 @@
+package org.mule.jenkins.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adcarabajal
+ * Date: 7/25/12
+ * Time: 3:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChangeSetItem {
+}
